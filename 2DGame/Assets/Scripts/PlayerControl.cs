d78cceb2fa02135d3c4a555feb8ae6baec9bc9df
TJ <tjfisher2015@gmail.com>
2019-04-13 17:51:39 -0600

    Animations

--- 2DGame/Assets/Scripts/PlayerControl.cs
+++ 2DGame/Assets/Scripts/PlayerControl.cs
@@ -34,21 +34,55 @@ public class PlayerControl : MonoBehaviour {
 	void Update () {
 		Walk();
 		Jump();
-		Sneak();
+		
 	}
 
 	void Walk(){
 		float moveSpeed = Input.GetAxis("Horizontal")*speed;
-		playerBody.velocity = new Vector2(moveSpeed,playerBody.velocity.y);
+		float sneakSpeed = moveSpeed/2;
+		float sprintSpeed = moveSpeed*1.5f;
+		float playerSpeed;
+
+		string cycleType;
+
+		//apparently switch statments don't work with inputs so ifelse it is
+		//Determine which movement speed and animation cycle should be applied
+		if(Input.GetButton("Sneak")){ //I have this set to leftctrl right now but unity interferes with it
+										//as in when pressing ctrl + D to sneak right, unity thinks you're trying to dupllicate the object
+										//Works with the alternate mouse3 though
+			playerSpeed = sneakSpeed;
+			//cycleType = "Sneak Cycle";
+			cycleType = "Walk Cycle";
+			Debug.Log(sneakSpeed + " " + moveSpeed);
+		}
+		else if(Input.GetButton("Sprint")){
+			playerSpeed = sprintSpeed;
+			//cycleType = "Sprint Cycle";
+			cycleType = "Walk Cycle";
+		}
+		else {
+			playerSpeed = moveSpeed;
+			cycleType = "Walk Cycle";
+		}
+
+		//Move the player
+		playerBody.velocity = new Vector2(playerSpeed,playerBody.velocity.y);
+
+
+
+
+		
+
 
 		//Should animations be moved to their own script?
+		
 		if (moveSpeed!=0) { // doesn't account for deadzones on controller but should be fine for now //also, you can change zones in input manager?
 			if(walkCycling==false){
-        		playerAnim.Play("Walk Cycle", 0, 0.6f);
+        		playerAnim.Play(cycleType, 0, 0.6f);//could use gamepad values to change speed but not needed for this class
 				walkCycling = true;
 			}
 			else if(walkCycling)
-				playerAnim.Play("Walk Cycle");
+				playerAnim.Play(cycleType);
 			if(moveSpeed<0)playerSprite.flipX = true;
 			else playerSprite.flipX = false;
 		}
@@ -58,11 +92,11 @@ public class PlayerControl : MonoBehaviour {
 		}
 	}
 
-	//Jump() and IsGrounded() could be moved to another script to provide access to other entities
+	//Jump() and IsGrounded() could be moved to another script to provide access to other entities potentially
 	void Jump(){
 		if(!IsGrounded()){return;}
 		float jump = Input.GetAxisRaw("Jump")*jumpHeight;
-		if(jump>0){
+		if(Input.GetButton("Jump")){
 			playerBody.velocity = new Vector2(playerBody.velocity.x,jump);
 			//grounded=false;
 			//play jump animation here
@@ -86,25 +120,25 @@ public class PlayerControl : MonoBehaviour {
     return false;
 	}
 
-	void Sneak (){
-		float sneakSpeed = Input.GetAxisRaw("Sneak")*speed/2;
-		if(sneakSpeed!=0){
-			playerBody.velocity = new Vector2(sneakSpeed,playerBody.velocity.y);
-			if(walkCycling==false){
-        		playerAnim.Play("Walk Cycle", 0, 0.6f);//change to sneak
-				walkCycling = true;
-			}
-			else if(walkCycling)
-				playerAnim.Play("Walk Cycle");//change to sneak
-			if(moveSpeed<0)playerSprite.flipX = true;
-			else playerSprite.flipX = false;
-		}
-    	else {
-    		playerAnim.Play("Idle");
-			walkCycling = false;
-		}
-		}
+	//combining in Walk() with 
+	// void Sneak (){
+	// 	float sneakSpeed = Input.GetAxisRaw("Sneak")*speed/2;
+	// 	if(sneakSpeed!=0){
+	// 		playerBody.velocity = new Vector2(sneakSpeed,playerBody.velocity.y);
+	// 		if(walkCycling==false){
+    //     		playerAnim.Play("Walk Cycle", 0, 0.6f);//change to sneak
+	// 			walkCycling = true;
+	// 		}
+	// 		else if(walkCycling)
+	// 			playerAnim.Play("Walk Cycle");//change to sneak
+	// 		if(moveSpeed<0)playerSprite.flipX = true;
+	// 		else playerSprite.flipX = false;
+	// 	}
+    // 	else {
+    // 		playerAnim.Play("Idle");
+	// 		walkCycling = false;
+	// 	}
 
 
-	}
+	// }
 }
