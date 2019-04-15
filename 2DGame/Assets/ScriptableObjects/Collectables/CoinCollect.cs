44398a53f1f7aa460eb87963d332fb275e70901c
TJ <tjfisher2015@gmail.com>
2019-04-15 17:19:28 -0600

    Added collectables

---
+++ 2DGame/Assets/ScriptableObjects/Collectables/CoinCollect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Coin", menuName = "Collectables/Coin")]
+public class CoinCollect : Collectables {
+	public int coinValue;
+
+	// Put spawning in collectableManager
+	// public override void Spawn(CollectableAI Collect){
+
+	// }
+	public override void Collect(CollectableAI pickUp){
+			pickUp.GetComponent<CollectableControl>().Collect(pickUp);
+
+	}
+
+
+}
+
+
+
+
---
+++ 2DGame/Assets/ScriptableObjects/Collectables/Collectables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Collectables : ScriptableObject {
+	public enum CollectableType {Coin, Arrow, PowerUp, Potion};
+
+	public string Name = "New Collectable";
+	public Sprite Sprite;
+	public int Amount;
+	//move to coinsSO
+	//public int cValue;
+	public CollectableType type;
+
+	//public virtual void Initialize(UnitAI unit){}
+	// public abstract void Spawn(CollectableAI Collect);
+	public abstract void Collect(CollectableAI pickUp);
+
+}
