02e2f46b4f1e04c1ed7df5855b41bc737af71c77
TJ <tjfisher2015@gmail.com>
2019-04-14 22:06:39 -0600

    Health bar addition

---
+++ 2DGame/Assets/Scripts/ImageFillSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ImageFillSetter : MonoBehaviour {
+	public FloatVariable variable;
+	public FloatVariable max; //see if there is a way to get this value from playerUnit
+
+
+	public Image image;
+
+
+	void Update () {
+
+		image.fillAmount = Mathf.Clamp01(variable.value/max.value);
+	}
+}
