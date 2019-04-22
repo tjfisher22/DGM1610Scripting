05f868c5b670020bd2d1956e48f378bf81c39764
TJ <tjfisher2015@gmail.com>
2019-04-22 09:59:50 -0600

    Arrows

---
+++ 2DGame/Assets/ScriptableObjects/Collectables/ArrowCollect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Coin", menuName = "Collectables/Arrow")]
+public class ArrowCollect : Collectables {
+	public enum ArrowType {Normal, Fire, Ice}
+	public ArrowType arrowType;
+
+
+
+	// Put spawning in collectableManager
+	// public override void Spawn(CollectableAI Collect){
+
+	// }
+	public override void Collect(CollectableAI pickUp){
+			pickUp.GetComponent<CollectableControl>().CollectValues(amount);
+
+			//put collectable specific stuff here
+			//Like add powerups, add to score.
+			//They will functions to call from CollectableControl I believe
+			//Nah, just pass important values into Collect()
+			//Or is that too much info for the class
+			//Keep it here if possible
+			//NOPE
+			//Send the important stuff 
+
+	}
+}
