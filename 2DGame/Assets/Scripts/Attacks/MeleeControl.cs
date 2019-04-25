28a426fac0fa8d3555c6247e37e33ee7bd6c03da
TJ <tjfisher2015@gmail.com>
2019-04-25 12:18:12 -0600

    Notes

--- 2DGame/Assets/Scripts/Attacks/MeleeControl.cs
+++ 2DGame/Assets/Scripts/Attacks/MeleeControl.cs
@@ -16,6 +16,7 @@ public class MeleeControl : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other){
 		//Debug.Log(other.gameObject);
 		//Debug.Log("Layer Name" + LayerMask.LayerToName(layersToKnockBack));
+		//should probable change this to a trigger enter
 		if(other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy"){
 			Debug.Log("Worked");
 			other.gameObject.GetComponent<UnitControl>().TakeDamage(weapon.damage,-gameObject.transform.localScale.x,weapon.knockBack);
